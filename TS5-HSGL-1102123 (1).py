394048aeea614bb8a7d9c94ee9da3727293d6247
Hector1102123 <143530963+Hector1102123@users.noreply.github.com>
2023-09-07 08:06:37 -0600

    Add files via upload

---
+++ TS5-HSGL-1102123 (1).py
@@ -0,0 +1,18 @@
+#Introducción a la programación sección 18#
+#7/9/2023#
+#Héctor Sebastián Garrido López"
+
+print("Mi segundo programa")
+Nombre=input("Ingrese su nombre")
+print("Nombre:"+ Nombre)
+
+edad=input("Ingrese su eded")
+print("edad:"+ edad)
+
+carrera=input("Ingrese carrera")
+print("carrera:"+ carrera)
+
+carné=input("Ingrese su carné")
+print("Nombre:"+ carné)
+
+print("Soy" + Nombre +",tengo"+edad +"años y estudio la carrera de"+ carrera +", mi número de carné es;"+carné)
